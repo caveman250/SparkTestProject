3113ceb164c225871ad765cdce7cd8cdf6024365
Jason Wood <jwood@startmail.com>
2024-10-04 21:46:32 +1000

    WIP tree view

--- app/src/UITestSystem.cpp
+++ app/src/UITestSystem.cpp
@@ -1,19 +1,15 @@
 #include "spark.h"
 
-#include <engine/math/Mat4.h>
-#include <engine/render/Material.h>
-#include <engine/ecs/components/MeshComponent.h>
 #include "UITestSystem.h"
 
-#include "TestApplication.h"
 #include "engine/Application.h"
-#include "engine/asset/AssetManager.h"
 #include "engine/ecs/components/RootComponent.h"
 #include "engine/ui/components/ButtonComponent.h"
-#include "engine/ui/components/ImageComponent.h"
 #include "engine/ui/components/TextComponent.h"
 #include "engine/ui/components/TitleBarComponent.h"
+#include "engine/ui/components/TreeViewComponent.h"
 #include "engine/ui/components/WindowComponent.h"
+#include "engine/ui/util/TreeViewUtil.h"
 #include "engine/ui/util/WindowUtil.h"
 
 using namespace se;
@@ -27,7 +23,6 @@ namespace app
     {
         auto app = Application::Get();
         auto world = app->GetWorld();
-        auto* assetManager = asset::AssetManager::Get();
 
         ui::components::RectTransformComponent* windowTransform;
         ui::components::WindowComponent* windowComp;
@@ -40,19 +35,31 @@ namespace app
         windowTransform->minY = 200;
         windowTransform->maxY = 720;
 
-        ecs::Id textEntity = world->CreateEntity();
-        auto textTransform = world->AddComponent<ui::components::RectTransformComponent>(textEntity);
-        textTransform->anchors = { 0.f, 1.f, 0.f, 1.f };
-        textTransform->minX = 0;
-        textTransform->maxX = 0;
-        textTransform->minY = 0;
-        textTransform->maxY = 0;
-        auto text = world->AddComponent<ui::components::TextComponent>(textEntity);
-        text->font = assetManager->GetAsset<asset::Font>("/builtin_assets/fonts/Arial.sass");
-        text->fontSize = 24;
-        text->text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies sed felis et pulvinar. Etiam tincidunt magna eget faucibus venenatis.";
+        ui::components::TreeViewComponent* treeViewComp = nullptr;
+        ui::components::RectTransformComponent* transformComp = nullptr;
+        auto treeView = ui::util::CreateTreeView(&treeViewComp, &transformComp);
+        world->AddChild(childArea, treeView);
+        transformComp->anchors = { 0.f, 1.f, 0.f, 1.f };
 
-        world->AddChild(childArea, textEntity);
+        ui::components::TreeNodeComponent* treeNodeComp = nullptr;
+        ui::components::TextComponent* textComp = nullptr;
+        auto treeNode = ui::util::InsertTreeNode(treeView, treeViewComp, treeView, &treeNodeComp, &textComp);
+        textComp->text = "one";
+
+        ui::components::TreeNodeComponent* treeNodeComp2 = nullptr;
+        ui::components::TextComponent* textComp2 = nullptr;
+        auto treeNode2 = ui::util::InsertTreeNode(treeView, treeViewComp, treeNode, &treeNodeComp2, &textComp2);
+        textComp2->text = "two";
+
+        ui::components::TreeNodeComponent* treeNodeComp3 = nullptr;
+        ui::components::TextComponent* textComp3 = nullptr;
+        ui::util::InsertTreeNode(treeView, treeViewComp, treeNode, &treeNodeComp3, &textComp3);
+        textComp3->text = "three";
+
+        ui::components::TreeNodeComponent* treeNodeComp4 = nullptr;
+        ui::components::TextComponent* textComp4 = nullptr;
+        ui::util::InsertTreeNode(treeView, treeViewComp, treeNode2, &treeNodeComp4, &textComp4);
+        textComp4->text = "four";
     }
 
     void UITestSystem::OnUpdate(const std::vector<ecs::Id>& , ui::components::RectTransformComponent*)
