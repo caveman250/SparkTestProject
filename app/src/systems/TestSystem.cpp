8fc72f9504efdfbf5ecf091b160bd698a353fc79
Jason Wood <jwood@startmail.com>
2026-01-05 17:30:57 +1100

    Cleanup

--- app/src/systems/TestSystem.cpp
+++ app/src/systems/TestSystem.cpp
@@ -1,14 +1,9 @@
 #include "spark.h"
 
 #include "TestSystem.h"
-#include "components/FirstPersonCameraComponent.h"
 #include "engine/Application.h"
 #include "engine/ecs/components/MeshComponent.h"
 #include "engine/ecs/components/TransformComponent.h"
-#include "engine/input/InputSystem.h"
-#include "engine/render/components/PointLightComponent.h"
-#include "engine/ui/components/ButtonComponent.h"
-#include "engine/ui/components/RectTransformComponent.h"
 
 using namespace se;
 using namespace se::ecs::components;
@@ -19,19 +14,15 @@ namespace app
     {
         return ecs::SystemDeclaration("TestSystem")
             .WithComponent<TransformComponent>()
-            .WithComponent<const MeshComponent>()
-            .WithSingletonComponent<input::InputComponent>();
+            .WithComponent<const MeshComponent>();
     }
-
-    static ecs::Id s_Scene = {};
-    static bool hasDestroyedScene = false;
 
     void TestSystem::OnInit(const ecs::SystemUpdateData&)
     {
         const auto app = Application::Get();
         const auto world = app->GetWorld();
 
-        s_Scene = world->LoadScene("/assets/scenes/test_scene.sass");
+        world->LoadScene("/assets/scenes/test_scene.sass");
         // std::function<void()> cb = [entity]()
         // {
         //     auto transform = Application::Get()->GetWorld()->GetComponent<TransformComponent>(entity);
@@ -55,13 +46,6 @@ namespace app
             {
                 transformComp.rot.y -= 360.f;
             }
-        }
-
-        auto input = updateData.GetSingletonComponent<input::InputComponent>();
-        if (!hasDestroyedScene && input->keyStates[static_cast<int>(input::Key::Backspace)] == input::KeyState::Down)
-        {
-            Application::Get()->GetWorld()->SaveScene(s_Scene, "/save/test_scene.json");
-            hasDestroyedScene = true;
         }
     }
 }
