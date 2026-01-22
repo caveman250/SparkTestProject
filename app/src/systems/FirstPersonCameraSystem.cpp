d0376f15387198a24f4966076640c00ff20d4460
Jason Wood <jwood@startmail.com>
2026-01-22 21:58:58 +1100

    Cleanup gizmo system and fix non editor builds.

--- app/src/systems/FirstPersonCameraSystem.cpp
+++ app/src/systems/FirstPersonCameraSystem.cpp
@@ -5,6 +5,7 @@
 #include "../components/FirstPersonCameraComponent.h"
 #include "engine/Application.h"
 #include "engine/camera/ActiveCameraComponent.h"
+#include "engine/ecs/components/TransformComponent.h"
 #include "engine/ecs/util/SystemUtil.h"
 #include "engine/input/InputComponent.h"
 #include "engine/input/InputUtil.h"
@@ -17,6 +18,7 @@ namespace app
     {
         return ecs::SystemDeclaration("First Person Camera System")
             .WithComponent<FirstPersonCameraComponent>()
+            .WithComponent<ecs::components::TransformComponent>()
             .WithSingletonComponent<camera::ActiveCameraComponent>()
             .WithSingletonComponent<input::InputComponent>();
     }
@@ -27,6 +29,8 @@ namespace app
         auto* activeCamera = updateData.GetSingletonComponent<camera::ActiveCameraComponent>();
         auto* input = updateData.GetSingletonComponent<input::InputComponent>();
         auto* cameras = updateData.GetComponentArray<FirstPersonCameraComponent>();
+        auto* transforms = updateData.GetComponentArray<ecs::components::TransformComponent>();
+
         if (entities.empty())
         {
             return;
@@ -44,6 +48,7 @@ namespace app
         {
             const auto& entity = entities[i];
             auto& camera = cameras[i];
+            auto& transform = transforms[i];
             input::InputUtil::ProcessMouseEvents(entity, input, [&camera](const input::MouseEvent& mouseEvent)
             {
                 if (mouseEvent.button == input::MouseButton::Left)
@@ -66,44 +71,46 @@ namespace app
             if (camera.mouseDown)
             {
                 auto dt = app->GetDeltaTime();
-                activeCamera->rot = activeCamera->rot + math::Vec3(dy * dt, dx * dt, 0.0f);
+                transform.rot = transform.rot + math::Vec3(dy * dt, dx * dt, 0.0f);
             }
 
-            math::Vec3 forward(cos(activeCamera->rot.x) * sin(activeCamera->rot.y),
-                         sin(activeCamera->rot.x),
-                         cos(activeCamera->rot.x) * cos(activeCamera->rot.y));
+            math::Vec3 forward(cos(transform.rot.x) * sin(transform.rot.y),
+                         sin(transform.rot.x),
+                         cos(transform.rot.x) * cos(transform.rot.y));
 
             math::Vec3 right = math::Vec3(
-                sin(activeCamera->rot.y - 3.14f / 2.0f),
+                sin(transform.rot.y - 3.14f / 2.0f),
                 0,
-                cos(activeCamera->rot.y - 3.14f / 2.0f));
+                cos(transform.rot.y - 3.14f / 2.0f));
 
             math::Vec3 up = math::Cross(right, forward);
 
             // Movement
             if (input->keyStates.at(static_cast<int>(input::Key::W)) == input::KeyState::Down)
             {
-                activeCamera->pos += forward * 5.f * Application::Get()->GetDeltaTime();
+                transform.pos += forward * 5.f * Application::Get()->GetDeltaTime();
             }
 
             if (input->keyStates.at(static_cast<int>(input::Key::S)) == input::KeyState::Down)
             {
-                activeCamera->pos -= forward * 5.f * Application::Get()->GetDeltaTime();
+                transform.pos -= forward * 5.f * Application::Get()->GetDeltaTime();
             }
 
             if (input->keyStates.at(static_cast<int>(input::Key::A)) == input::KeyState::Down)
             {
-                activeCamera->pos -= right * 5.f * Application::Get()->GetDeltaTime();
+                transform.pos -= right * 5.f * Application::Get()->GetDeltaTime();
             }
 
             if (input->keyStates.at(static_cast<int>(input::Key::D)) == input::KeyState::Down)
             {
-                activeCamera->pos += right * 5.f * Application::Get()->GetDeltaTime();
+                transform.pos += right * 5.f * Application::Get()->GetDeltaTime();
             }
 
+            activeCamera->pos = transform.pos;
+            activeCamera->rot = transform.rot;
             activeCamera->view = math::LookAt(
-                    activeCamera->pos,
-                    activeCamera->pos + forward,
+                    transform.pos,
+                    transform.pos + forward,
                     up);
 
             auto gameViewport = app->GetGameViewportSize();
